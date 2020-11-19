3c40b98bb5d81b1bb8ecc5e658a9374124825c9b
mikezimm <49648086+mikezimm@users.noreply.github.com>
2020-11-19 12:10:33 -0500

    Setup baseline webpart

---
+++ src/webparts/pivottiles7/components/IPivottiles7Props.ts
@@ -0,0 +1,3 @@
+export interface IPivottiles7Props {
+  description: string;
+}
---
+++ src/webparts/pivottiles7/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPivottiles7WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'Pivottiles7WebPartStrings' {
+  const strings: IPivottiles7WebPartStrings;
+  export = strings;
+}
