778a00c06126391ba16684ffece5ea490656063b
mikezimm <49648086+mikezimm@users.noreply.github.com>
2020-12-17 14:33:26 -0500

    Updated standard functions from zFunctions

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,3 +1,22 @@
+/***
+ *    .d888b.  .d88b.  .d888b.  .d88b.          db .d888b.         db   j88D                     
+ *    VP  `8D .8P  88. VP  `8D .8P  88.        o88 VP  `8D        o88  j8~88                     
+ *       odD' 88  d'88    odD' 88  d'88         88    odD'         88 j8' 88                     
+ *     .88'   88 d' 88  .88'   88 d' 88 C8888D  88  .88'   C8888D  88 V88888D                    
+ *    j88.    `88  d8' j88.    `88  d8'         88 j88.            88     88                     
+ *    888888D  `Y88P'  888888D  `Y88P'          VP 888888D         VP     VP                     
+ *                                                                                               
+ *                                                                                               
+ *    d8888b. d888888b db    db  .d88b.  d888888b      d888888b d888888b db      d88888b .d8888. 
+ *    88  `8D   `88'   88    88 .8P  Y8. `~~88~~'      `~~88~~'   `88'   88      88'     88'  YP 
+ *    88oodD'    88    Y8    8P 88    88    88            88       88    88      88ooooo `8bo.   
+ *    88~~~      88    `8b  d8' 88    88    88            88       88    88      88~~~~~   `Y8b. 
+ *    88        .88.    `8bd8'  `8b  d8'    88            88      .88.   88booo. 88.     db   8D 
+ *    88      Y888888P    YP     `Y88P'     YP            YP    Y888888P Y88888P Y88888P `8888Y' 
+ *                                                                                               
+ *                                                                                               
+ */
+
 import { IUser } from './IReUsableInterfaces';
 
 import { sp } from "@pnp/sp";
@@ -244,11 +263,12 @@ export async function ensureTheseUsers ( theseUsers: IUser[], checkTheseUsers: I
     let thisWeb = Web(webUrl);
 
     let errMessage = null;
-    let adminFilter = "IsSiteAdmin eq true";
+    //let adminFilter = "IsSiteAdmin eq true"; //This did not work....
+    let adminFilter = "IsSiteAdmin eq 1";  //Updated per @koltyakov: https://github.com/pnp/pnpjs/issues/1480
 
     try {
  
-      const siteAdmins = await thisWeb.siteUserInfoList.items.filter(`IsSiteAdmin eq 1`).get();
+      const siteAdmins = await thisWeb.siteUserInfoList.items.filter( adminFilter ).get();
 
       /**
        * This was added because loginName is not retured but is in other functions so it just copies it to make it easier to resuse.
@@ -263,9 +283,7 @@ export async function ensureTheseUsers ( theseUsers: IUser[], checkTheseUsers: I
     } catch (e) {
 
       errMessage = getHelpfullError(e, true, true);
-      if ( supressError === true && errMessage.indexOf('Save Conflict') === 0 ) {
-        //Do nothting
-      } else {
+      if ( supressError !== true ) {
         alert( errMessage );
       }
       console.log( 'getSiteAdmins', errMessage );
